49e1b01c4618748ac5c41b0832c78586516af63c
duc.nguyen <ducnta.work@gmail.com>
2026-02-26 18:46:40 +0900

    only access page on authenticate

--- src/routes/(auth)/sign-in.tsx
+++ src/routes/(auth)/sign-in.tsx
@@ -1,12 +1,19 @@
 import { z } from 'zod'
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
 import { SignIn } from '@/features/auth/sign-in'
+import { supabaseAuth } from '@/services/supabase'
 
 const searchSchema = z.object({
   redirect: z.string().optional(),
 })
 
 export const Route = createFileRoute('/(auth)/sign-in')({
+  beforeLoad: async () => {
+    const session = await supabaseAuth.getSession()
+    if (session) {
+      throw redirect({ to: '/' })
+    }
+  },
   component: SignIn,
   validateSearch: searchSchema,
 })
